cf2b561f5f724b4bd1b10f2f5ef5b63057198835
thijnmens <thijnsmulders04@gmail.com>
2025-04-27 16:17:28 +0200

    Add Playing card and Player Info Card

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { countryCodeEmoji } from 'country-code-emoji';
 import GameState from './messages/gameState';
 import Handshake from './messages/handshake';
 import MapInfo from './messages/mapInfo';
@@ -7,29 +8,136 @@ import Score from './messages/score';
 import OverlayCore from './overlay-core';
 
 const overlay = new OverlayCore();
+let playerId = '';
 
 overlay.onGameState = (gameState: GameState) => {
-	console.log(gameState.toString());
+	if (gameState.gameStateChanged == 'Menu') {
+		updatePlayerCard(playerId);
+		document.getElementById('map-card')!.style =
+			'transform: translateX(-100%);transition-timing-function: cubic-bezier(0.4, 0, 1, 1);';
+		setTimeout(() => {
+			document.getElementById('player-card')!.style =
+				'transform: translateX(0px);transition-timing-function: cubic-bezier(0, 0, 0.2, 1);';
+		}, 1000);
+	} else {
+		document.getElementById('player-card')!.style =
+			'transform: translateX(-100%);transition-timing-function: cubic-bezier(0.4, 0, 1, 1);';
+		setTimeout(() => {
+			document.getElementById('map-card')!.style =
+				'transform: translateX(0px);transition-timing-function: cubic-bezier(0, 0, 0.2, 1);';
+		}, 1000);
+	}
+};
+
+const updatePlayerCard = (playerId: string, full: boolean = false) => {
+	fetch(
+		`https://api.beatleader.xyz/player/${playerId}?stats=true&keepOriginalId=false&leaderboardContext=none`,
+		{
+			method: 'GET',
+			mode: 'cors'
+		}
+	)
+		.then((r) => r.json())
+		.then((data) => {
+			if (full) {
+				document.getElementById('player-name')!.innerHTML = data.name;
+				if (data.clans.length > 0) {
+					document.getElementById('player-clan')!.innerHTML =
+						data.clans[0].tag;
+					document.getElementById('player-clan')!.style.outlineColor =
+						data.clans[0].color;
+				}
+				(document.getElementById(
+					'player-pfp'
+				) as HTMLImageElement | null)!.src = data.avatar;
+			}
+
+			document.getElementById('global-rank')!.innerHTML =
+				`🌐 #${data.rank}`;
+			document.getElementById('country-rank')!.innerHTML =
+				`${countryCodeEmoji(data.country)} #${data.countryRank}`;
+			document.getElementById('avg-rank')!.innerHTML =
+				`avg #${Math.ceil(data.scoreStats.averageRank).toString()}`;
+			document.getElementById('ssplus-count')!.innerHTML =
+				`SS+ | ${data.scoreStats.sspPlays}`;
+			document.getElementById('ss-count')!.innerHTML =
+				`SS | ${data.scoreStats.ssPlays}`;
+			document.getElementById('splus-count')!.innerHTML =
+				`S+ | ${data.scoreStats.spPlays}`;
+			document.getElementById('s-count')!.innerHTML =
+				`S | ${data.scoreStats.sPlays}`;
+			document.getElementById('a-count')!.innerHTML =
+				`A | ${data.scoreStats.aPlays}`;
+		});
 };
 
 overlay.onHandshake = (handshake: Handshake) => {
-	console.log(handshake.toString());
+	playerId = handshake.playerPlatformId;
+	updatePlayerCard(handshake.playerPlatformId, true);
 };
 
 overlay.onMapInfo = (mapInfo: MapInfo) => {
-	console.log(mapInfo.toString());
+	let diff = 'expert+';
+	let diffColor = '#8352d5';
+
+	switch (mapInfo.mapInfoChanged.difficulty) {
+		case 'Easy':
+			diff = 'easy';
+			diffColor = '#69af78';
+			break;
+
+		case 'Normal':
+			diff = 'normal';
+			diffColor = '#75aff0';
+			break;
+
+		case 'Hard':
+			diff = 'hard';
+			diffColor = '#e87050';
+			break;
+
+		case 'Expert':
+			diff = 'expert';
+			diffColor = '#ab3c44';
+			break;
+	}
+
+	document.getElementById('title')!.innerHTML = mapInfo.mapInfoChanged.name;
+	document.getElementById('artist')!.innerHTML =
+		mapInfo.mapInfoChanged.artist;
+	document.getElementById('mapper')!.innerHTML =
+		`[${mapInfo.mapInfoChanged.mapper}]`;
+	document.getElementById('diff')!.innerHTML = diff;
+	document.getElementById('diff')!.style.backgroundColor = diffColor;
+	document.getElementById('pp')!.innerHTML =
+		`${mapInfo.mapInfoChanged.PP} pp`;
+	(document.getElementById('cover') as HTMLImageElement | null)!.src =
+		`data:image/png;base64,${mapInfo.mapInfoChanged.coverRaw}`;
+
+	if (
+		document.getElementById('title')!.offsetWidth >
+		document.getElementById('title-container')!.offsetWidth
+	) {
+		document.getElementById('title')!.style.animation =
+			'scroll-text 10s ease-in infinite';
+	} else {
+		document.getElementById('title')!.style.animation = '';
+	}
 };
 
 overlay.onPause = (pause: Pause) => {
-	console.log(pause.toString());
+	document.getElementById('pause')!.style.visibility = 'visible';
 };
 
 overlay.onResume = (resume: Resume) => {
-	console.log(resume.toString());
+	document.getElementById('pause')!.style.visibility = 'hidden';
 };
 
 overlay.onScore = (score: Score) => {
-	console.log(score.toString());
+	document.getElementById('acc')!.innerHTML =
+		`${(score.scoreEvent.accuracy * 100).toFixed(2)}%`;
+	document.getElementById('miss')!.innerHTML =
+		`${score.scoreEvent.missCount} Miss`;
 };
 
 overlay.init();
